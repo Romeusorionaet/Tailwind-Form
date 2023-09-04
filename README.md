162938523b73af475b66df019b9e5829e033f740
romeusorionaet <romeusoares14569@gmail.com>
2023-09-04 11:01:20 -0300

    add: readme.md / fix: create component for theme providers

---
+++ README.md
@@ -0,0 +1,47 @@
+<h1 align='center'> Tailwind Form </h1>
+
+<p>📃 Página dedicado a prática do Tailwindcss, nele criei um formulário responsivo como sidebar e tema dark mode. Cofere no link abaixo.</p>
+
+<div align='center'>
+
+[Entre no Space Time](https://nlw-space-time-web-nu.vercel.app)
+
+</div>
+
+<div align='center'>
+
+![preview](./src/assets/previewsPage/previewDesktopTailwindForm.png)
+
+![preview](./src/assets/previewsPage/previewMobileTailwindForm.png)
+
+</div>
+
+<h2> 🛠 Principais Tecnologias Utilizadas </h2>
+
+- reactjs
+- nextjs
+- typescript
+- tailwindcss
+- next-themes
+- radix-ui
+
+<h2 align='center'> Meus contatos </h2>
+
+* [LinkedIn](https://www.linkedin.com/in/romeu-soares-87749a231/)
+
+* romeuindexjs@gmail.com
+
+* WhatsApp: 84 981127596
+
+<br />
+
+<h3> Autor </h3>
+
+>Romeu soares de souto
+
+<h3> Licença </h3>
+
+<p>
+<img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+</p>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,13 @@
-'use client'
-
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Sidebar } from './components/Sidebar'
-import { ThemeProvider } from 'next-themes'
+import { Providers } from './components/ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Tailwindcss + Nextjs',
+  title: 'Tailwindcss Form',
   description: 'Classroom Project',
 }
 
@@ -21,14 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en" className="antialiased">
       <body className={inter.className}>
-        <ThemeProvider attribute="class">
+        <Providers>
           <div className="min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app">
             <Sidebar />
             <main className="px-4 pb-24 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
               {children}
             </main>
           </div>
-        </ThemeProvider>
+        </Providers>
       </body>
     </html>
   )
